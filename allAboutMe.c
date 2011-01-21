d6cf06dd5b2961ebd2bbcfb50aa764ae6ffe0aa2
Hoatlin <hoatlind@arch07.cis.gvsu.edu>
2011-01-21 12:36:38 -0500

    permissions working (kind of)

--- allAboutMe.c
+++ allAboutMe.c
@@ -20,12 +20,14 @@
    Due:    Jan. 27 2011 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pwd.h>
 #include <grp.h>
 #include <sys/stat.h>
 
 void getUserInfo();
 void getEntInfo();
+void getPermissions(char* premissions, int mode);
 
 int main()
 {
@@ -46,21 +48,89 @@ void getUserInfo()
 	struct stat st;
 
 	char *user;
+	char permissions[8];
 
 	user = getenv("USER");
 	
 	pw = getpwnam(user);
 	gr = getgrgid(pw->pw_gid);
 	stat(pw->pw_dir, &st);
-	
+
+	getPermissions(permissions, st.st_mode);
 
 	printf("Unix User       : %s (%d)\n", pw->pw_name, pw->pw_uid);
 	printf("Name            : %s\n", pw->pw_gecos);
 	printf("Unix Group      : %s (%d)\n", gr->gr_name, gr->gr_gid);
 	printf("Unix Home       : %s\n", pw->pw_dir);
-	printf("Home Permission : %d\n", st.st_mode);
+	printf("Home Permission : %s\n", permissions);
 	printf("Login Shell     : %s\n", pw->pw_shell);
 }
+
+void getPermissions(char* permissions, int mode)
+{
+	//currently this is the best way i can think of doing this
+
+	//read write execute for user	
+	if(mode & S_IRUSR){
+		strcat(permissions, "r");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IWUSR){
+		strcat(permissions, "w");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IXUSR){
+		strcat(permissions, "x");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+
+	//read write execute for group
+	if(mode & S_IRGRP){
+		strcat(permissions, "r");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IWGRP){
+		strcat(permissions, "w");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IXGRP){
+		strcat(permissions, "x");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+
+	//read write execute for others
+	if(mode & S_IROTH){
+		strcat(permissions, "r");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IWOTH){
+		strcat(permissions, "w");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+	if(mode & S_IXOTH){
+		strcat(permissions, "x");
+	}
+	else{
+		strcat(permissions, "-");
+	}
+}
+
 
 //this is starting with root. probably cycles through all users
 //will need for showing sharing end char.
